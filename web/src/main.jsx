e64f4c015f7eda3f65a876056729bc2e6cefefb5
kolik29 <kolik29@gmail.com>
2026-01-09 22:32:03 +0300

    ios resize fix

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,12 +3,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { MantineProvider } from "@mantine/core";
+import { createTheme, MantineProvider } from "@mantine/core";
 import "./global.sass";
+
+const theme = createTheme({
+  components: {
+    TextInput: {
+      styles: {
+        input: {
+          fontSize: 16,
+        },
+      },
+    },
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <App />
     </MantineProvider>
   </StrictMode>,
